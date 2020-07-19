01bb5359e8baa2a566cd6b6171efcf302ef8e19b
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-19 01:43:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,6 +69,12 @@ end
 
 def current_player(board)
  if turn_count(board) % 3 != 0 || turn_count(board) % 2 != 0
+   return "XY"
+ elsif turn_count(board) % 3 == 0
    return "X"
+ else
+   "Y"
+ end
+
  end
 end
